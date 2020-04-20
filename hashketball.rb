c5477a5ee4a79ae9cebbb4c6a57708e6637cd843
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-04-20 01:06:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -136,7 +136,7 @@ end
 def num_points_scored(player_name)
   game_hash.each do |location, team_data|
     team_data.each do |attribute, data|
-      data.each do |data_item|
+      data.each do |player_name|
         puts data_item
       end
     end
