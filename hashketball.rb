65047842783fb744fe3e9c890fe55cf64b4682ed
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-04-20 02:24:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -172,7 +172,6 @@ def player_numbers(team_name)
   game_hash.each do |location, team_data|
     if team_data[:team_name] == team_name
       team_data[:players].each do |player|
-        binding.pry
       player_numbers << player[:number]
         end
       end
